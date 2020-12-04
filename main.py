c5531c367286901055e3f21c05f525d87f457f23
Jonah Philion <jonahphilion@Jonahs-MBP-4.attlocal.net>
2020-12-04 11:13:39 -0600

    fixed tet/tri issue

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from simple_3dviz.utils import render
 from simple_3dviz.behaviours.io import SaveFrames
 from simple_3dviz.behaviours.trajectory import Circle
 from simple_3dviz.behaviours.movements import CameraTrajectory
+from numba import jit
 
 
 def read_file(fname):
@@ -41,8 +42,35 @@ def get_grid(data, ndisc, buffer=0.1):
     return grid.view(-1, 3), dx
 
 
-def sign_unsigned(fname='./data/elephant.pwn', K=12, ndisc=70,
-                  debug_viz=True):
+def get_trivertices(tetvertices):
+    return torch.cat((
+        tetvertices[:, [0, 1, 2]],
+        tetvertices[:, [1, 2, 3]],
+        tetvertices[:, [2, 3, 0]],
+        tetvertices[:, [3, 0, 1]],
+    ), 0)
+
+
+# def estimate_sign(ix, points, vertices, is_in_band, R):
+#     for _ in range(R):
+#         traj = []
+#         vec = np.random.normal(size=3)
+#         vec / np.linalg.norm(vec)
+        
+#         traj.append(is_in_band[ix])
+#         while True:
+
+
+# def get_v2v(vertices):
+#     """v (int) -> set(v0, v1,...) (ints)
+#     """
+#     print('hello', len(np.unique(vertices.flatten())))
+#     print(np.max(vertices) + 1)
+#     # return v2v
+
+
+def sign_unsigned(fname='./data/elephant.pwn', K=5, ndisc=50,
+                  debug_viz=True, R=10):
     # read in data (N x 3)
     print('reading data...')
     data = read_file(fname)
@@ -66,25 +94,40 @@ def sign_unsigned(fname='./data/elephant.pwn', K=12, ndisc=70,
     print('delaunay...')
     M = DelaunayTri(xyzpts.numpy())
     points = torch.Tensor(M.points)
-    vertices = torch.LongTensor(M.vertices)
+    tetvertices = torch.LongTensor(M.vertices)
+    trivertices = get_trivertices(tetvertices)
+
+    # choose epsilon
+    eixes = dist.sort().values[torch.linspace(0, len(dist) - 1, 50).long()]
+    epsilon = eixes[int(len(eixes) * 0.5)]
+
+    # coarse sign estimate
+    # print('sign estimate...')
+    # v2v = get_v2v(vertices.numpy())
+    # outside_ixes = torch.arange(len(dist))[dist >= epsilon]
+    # is_in_band = dist < epsilon
+    # for ix in outside_ixes:
+    #     estimate_sign(ix.item(), points.numpy(), vertices.numpy(), is_in_band.numpy(), R)
 
 
     ### Visualization
 
     def debug_viz():
         print('plotting...')
-        for epi, epsilon in enumerate(torch.linspace(dist.min(), dist.max(), 50)):
-            kept = (dist[vertices[:, 0]] < epsilon) & (dist[vertices[:, 1]] < epsilon) & (dist[vertices[:, 2]] < epsilon)
-            if kept.sum().item() == 0:
-                continue
-            # viewdata = torch.cat((xyzpts, data), 0)
-            # print(viewdata.shape, data.shape)
-            # s = Spherecloud(viewdata.numpy(), sizes=[0.005 for _ in range(viewdata.shape[0])])
+        # eixes = dist.sort().values[torch.linspace(0, len(dist) - 1, 50).long()]
+        # for epi, epsilon in enumerate(eixes):
+        kept = (dist[trivertices[:, 0]] < epsilon) & (dist[trivertices[:, 1]] < epsilon) & (dist[trivertices[:, 2]] < epsilon)
+        # if kept.sum().item() < 1:
+        #     continue
+        # viewdata = torch.cat((xyzpts, data), 0)
+        # print(viewdata.shape, data.shape)
+        # s = Spherecloud(viewdata.numpy(), sizes=[0.005 for _ in range(viewdata.shape[0])])
 
-            m = Mesh.from_faces(points.numpy(), vertices.numpy()[kept][:, :3], colors=np.ones((len(points), 3))*[1.0, 0.0, 0.0])
-            c = Circle(center=(0, 0, 0), point=(5, 0, 0.), normal=(0, 0, 1))
-            ctrj = CameraTrajectory(c, speed=0.02)
-            render(m, behaviours=[ctrj, SaveFrames(f"./frame_{epi:03}_{{:03d}}.png", every_n=4)], n_frames=60)
+        m = Mesh.from_faces(points.numpy(), trivertices.numpy()[kept], colors=np.ones((len(points), 3))*[1.0, 0.0, 0.0])
+        c = Circle(center=(0, 0, 0), point=(5, 0, 0.), normal=(0, 0, 1))
+        ctrj = CameraTrajectory(c, speed=0.005)
+        # render(m, behaviours=[ctrj, SaveFrames(f"./frame_{epi:03}_{{:03d}}.png", every_n=5)], n_frames=100)
+        show(m)
 
     if debug_viz:
         debug_viz()
